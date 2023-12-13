5b695b80eeb944bbcdaa790431741cf2087de861
patliemoaloo <aloemo77@gmail.com>
2023-12-13 11:54:39 +0300

    Method to create a customer

--- alchemy/lib/main.py
+++ alchemy/lib/main.py
@@ -1,4 +1,22 @@
 from config import *
 
 # CRUD OPERATIONS FOR THE USER
-# Testing my branch
+
+# Create of CRUD
+
+def create_customer(first_name, last_name, email, contact):
+    
+    if not isinstance(contact, int) or not (1000000000 <= contact <= 9999999999):
+        print("Kindly enter a valid 10-digit phone number")
+        return None
+    elif not isinstance(first_name, str) or not isinstance(last_name, str):
+        print("Kindly enter a valid name")
+        return None
+    
+    customer = Customer(first_name = first_name, last_name = last_name, email = email, contact = contact)
+    
+    session.add(customer)
+    session.commit()
+    
+    return customer
+
